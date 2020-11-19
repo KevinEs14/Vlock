74364f071df39a88d227b11ea24438469e2eb3d2
pjalvin <pomaalvin@gmail.com>
2020-11-19 03:23:26 -0400

    se agrego el acceso con voz y la animacion para el microfono

--- lib/login.dart
+++ lib/login.dart
@@ -1,9 +1,152 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:local_auth/auth_strings.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:simple_animations/simple_animations.dart';
 import 'package:vlock/voicePage.dart';
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
 
-class LoginPage extends StatelessWidget {
+}
+class _LoginPageState extends State<LoginPage>{
+  final LocalAuthentication auth = LocalAuthentication();
+  bool _canCheckBiometrics;
+  List<BiometricType> _availableBiometrics;
+  String _authorized = 'Not Authorized';
+  bool _isAuthenticating = false;
+  Future<void> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await auth.canCheckBiometrics;
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
+    });
+  }
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await auth.getAvailableBiometrics();
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+  Future<void> _authenticate() async {
+    bool authenticated = false;
+    try {
+      setState(() {
+        _isAuthenticating = true;
+        _authorized = 'Authenticating';
+      });
+      authenticated = await auth.authenticateWithBiometrics(
+        androidAuthStrings: AndroidAuthMessages(signInTitle: "Desbloqueo con huella",
+        fingerprintHint: "",cancelButton: "Utilizar contraseña"),
+          localizedReason: 'Utiliza la huella para despbloquear la aplicacion.',
+          useErrorDialogs: true,
+          stickyAuth: true);
+      setState(() {
+        _isAuthenticating = false;
+        _authorized = 'Authenticating';
+      });
+    } on PlatformException catch (e) {
+      print(e);
+    }
+    if (!mounted) {
+      print("asdfsadf");
+      return;}
+
+    final String message = authenticated ? 'Authorized' : 'Not Authorized';
+    setState(() {
+      _authorized = message;
+    });
+  }
+
+  void _cancelAuthentication() {
+    auth.stopAuthentication();
+  }
+  mostrarHuella()async {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await _authenticate();
+      if(_authorized=="Authorized"){
+        Navigator.push(context,MaterialPageRoute(builder: (context)=>VoicePage()));
+      }
+      /*showDialog(context: context,
+          builder: (BuildContext context){
+            return AlertDialog(
+              contentPadding: EdgeInsets.all(0),
+              content: Container(
+                width: size.width*0.8,
+                height: size.width*0.8,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage("assets/fondo_huella.jpg"),
+                      fit: BoxFit.fill
+                    )
+                  ),
+                  child: Stack(
+                    children: [
+                      Container(
+                        height: size.width*0.2,
+                        child:
+                          Center(
+                            child:
+                            Text("Inicio con huella"+_authorized,textAlign: TextAlign.center,style: TextStyle(fontSize: size.width*0.06,color: Colors.white),),
+                          )
+                      ),
+                     Container(
+                       child: Center(
+                         child:  AnimatedContainer(duration: Duration(milliseconds: 500),
+                           child: Icon(
+                             Icons.fingerprint_sharp,
+                             size: size.width*0.25,
+                             color: Color(0xff99091A),
+                           ),
+                         ),
+                       ),
+                     ),
+                      Container(
+                        margin: EdgeInsets.only(top: size.width*0.6),
+                        child:
+                        Center(
+                          child: RaisedButton(
+                            elevation: 0,
+                            child: Text("Ingresar con correo y contraseña",style:TextStyle(fontSize:size.width*0.04,color: Color(0xff095C99))),
+                            color: Colors.transparent,
+                            onPressed: (){
+                                Navigator.pop(context);
+                            },
+                          ),
+                        )
+                      )
+                    ],
+                  ),
+              ),
+            );
+          });*/
+    });
+    }
+
+  @override
+  initState(){
+    super.initState();
+    mostrarHuella();
+  }
   Size size;
+  TextEditingController nombre=TextEditingController();
+  TextEditingController password=TextEditingController();
   @override
   Widget build(BuildContext context) {
     size=MediaQuery.of(context).size;
@@ -74,13 +217,15 @@ class LoginPage extends StatelessWidget {
                   ],
                 ),
               ),
-              Padding(
-                  padding: EdgeInsets.all(30.0),
+              Container(
+                padding: EdgeInsets.all(30.0),
+                height: size.height*0.6,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     SizedBox(height: 20,),
                     Container(
-                      height: 70,
+                      height: 60,
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
                           color: Colors.white,
@@ -99,6 +244,7 @@ class LoginPage extends StatelessWidget {
                           // border: Border(bottom: BorderSide(color: Colors.grey))
                         ),
                         child: TextField(
+                          controller: nombre,
                           decoration: InputDecoration(
                               border: InputBorder.none,
                               hintText: "Nombre de usuario",
@@ -109,7 +255,7 @@ class LoginPage extends StatelessWidget {
                     ),
                     SizedBox(height: 30,),
                     Container(
-                      height: 70,
+                      height: 60,
                       padding: EdgeInsets.all(5),
                       decoration: BoxDecoration(
                           color: Colors.white,
@@ -129,6 +275,8 @@ class LoginPage extends StatelessWidget {
                           // border: Border(bottom: BorderSide(color: Colors.grey))
                         ),
                         child: TextField(
+                          controller: password,
+                          obscureText: true,
                           decoration: InputDecoration(
                               border: InputBorder.none,
                               hintText: "Password",
@@ -140,20 +288,25 @@ class LoginPage extends StatelessWidget {
                     SizedBox(height: 30,),
                     GestureDetector(
                       onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (context)=>VoicePage()));
+                        if(password.text.toString()=="1234abc"&&nombre.text.toString()=="admin"){
+                          Navigator.push(context, MaterialPageRoute(builder: (context)=>VoicePage()));
+                        }
+                        else{
+                          Fluttertoast.showToast(msg: "Usuario o contraseña incorrectos",backgroundColor: Colors.black.withOpacity(0.5));
+                        }
                       },
                       child: Container(
-                        height: 60,
+                        margin: EdgeInsets.only(left: size.width*0.2,right: size.width*0.2),
+                        height: 40,
                         decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(20.0),
                           gradient: LinearGradient(
                             colors: [
-                              Colors.blueAccent,
-                              Colors.redAccent
-                            ]
+                              Color(0xff121212),Color(0xff99091A),
+                              Color(0xff121212)]
                           ),
                         ),
-                        child: Center(child: Text("Login",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),)),
+                        child: Center(child: Text("Login",style: TextStyle(fontSize:size.width*0.05,color: Colors.white,fontWeight: FontWeight.bold),)),
                         ),
                     ),
                   ],
--- lib/voicePage.dart
+++ lib/voicePage.dart
@@ -1,27 +1,105 @@
-import 'package:flutter/material.dart';
+import 'dart:ffi';
 
-class VoicePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+class VoicePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _VoicePageState();
+  }
+
+}
+class _VoicePageState extends State<VoicePage> {
   Size size;
+  double tamMic=0.3;
+  int dir=1;
+  Color _color;
+
+  @override
+  initState(){
+    super.initState();
+    hilo();
+  }
+  cambiarTam(){
+
+    setState(() {
+      tamMic+=0.0015*dir;
+      if(tamMic>=0.45){
+        dir=-1;
+        _color=Colors.red.withOpacity(0.1);
+      }
+      else{
+        if(tamMic<=0.3){
+          dir=1;
+          _color=Colors.red.withOpacity(0.9);
+        }
+      }
+    });
+    hilo();
+  }
+  hilo()async{
+    await Future.delayed(Duration(milliseconds: 1));
+    cambiarTam();
+  }
   @override
   Widget build(BuildContext context) {
     size=MediaQuery.of(context).size;
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
+        body:Column(
           children: [
             SizedBox(height: size.height*0.3,),
-            Center(child: Text("Bienvenido, presione para desbloquear con la voz",style: TextStyle(color: Colors.black),)),
-            Container(
-              child: GestureDetector(
-                onTap: (){
-                  Navigator.pop(context);
-                },
-                child: Image(image: AssetImage("assets/images/voiceButton.jpg"),),
-              ),
+            Center(child: Text("Presione para escuchar el comando.",style: TextStyle(color: Colors.black),)),
+            Expanded(
+                child: Center(
+                  child: Stack(
+                    children: [
+                      Container(
+                      width: size.width,
+                      height: size.height*0.6,
+
+                      child: Center(
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 100),
+                          width: size.width*tamMic*0.8,
+                          height: size.width*tamMic*0.8,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(
+                                  size.width*0.5),
+                              border: Border.all(color: Colors.red.withOpacity(((tamMic-0.3)/0.15>1?1:(tamMic-0.3)/0.15<0?0:(tamMic-0.3)/0.15)*0.2))
+                          ),
+                        ),
+                      ),
+                    ),
+                      Container(
+                        width: size.width,
+                        height: size.height*0.6,
+                        
+                        child: Center(
+                          child: AnimatedContainer(
+                            duration: Duration(milliseconds: 1),
+                            width: size.width*tamMic,
+                            height: size.width*tamMic,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(
+                                    size.width*0.5),
+                                border: Border.all(color: Colors.red.withOpacity((tamMic-0.3)/0.15>1?1:(tamMic-0.3)/0.15<0?0:(tamMic-0.3)/0.15))
+                            ),
+                          ),
+                        ),
+                      ),
+
+                      //Image(image: AssetImage("assets/images/voiceButton.jpg"),),
+                      Container(
+                        width: size.width,
+                        height: size.height*0.6,
+                        child: Icon(Icons.mic_rounded,size: size.width*0.2,color: Color(0xff99091A)),
+                      )
+                    ],
+                  ),
+                )
             ),
           ],
         ),
-      ),
     );
   }
 }
